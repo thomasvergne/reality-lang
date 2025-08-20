a1fea8a9f5368a3116794b83bac4ff3925d8760b
Thomas Vergne <contact@thomas-vergne.fr>
2025-08-20 14:27:23 +0200

    feat(crates/ast): added annotation module

---
+++ crates/reality-ast/src/internal/annotation.rs
@@ -0,0 +1,22 @@
+// <==== ANNOTATION MODULE ====>
+// 
+// This module defines the Annotation struct, which represents annotated values
+// in the Reality language. Annotations are used to attach metadata, such as
+// names, types, and locations, to various AST nodes.
+//
+// <==== LITERAL MODULE ====>
+
+use std::fmt::Debug;
+
+pub struct Annotation<T> {
+  pub name: String,
+  pub value: T,
+  pub location: (usize, usize),
+}
+
+impl<T: Debug> Debug for Annotation<T> {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(f, "{}", self.name)?;
+    write!(f, ": {:?}", self.value)
+  }
+}
