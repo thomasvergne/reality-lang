4fc00721825892be228e74803dd946c761837d30
Thomas Vergne <contact@thomas-vergne.fr>
2025-08-20 13:08:47 +0200

    feat(crates/ast): added types definition

---
+++ crates/reality-ast/src/internal/types.rs
@@ -0,0 +1,80 @@
+// <==== TYPES MODULE ====>
+// 
+// This module defines the various types used in the Reality language.
+// Whereas values represent the building blocks of the language, types
+// provide the structure and constraints for those values, ensuring
+// that they are used correctly within the language's syntax and semantics.
+// 
+// The following types are defined by default in the Reality language:
+// 
+// (1). TypeIdentifier(String) may be represented by <id>.
+// 
+// (2). TypeApplication(Box<Type>, Vec<Type>) may be represented by 
+//      <base_type>[<arg1>, <arg2>, ...].
+// 
+// (3). TypeVariable(Box<TypeVariable>) may not be represented in source code. It
+//      represents a type variable that can be instantiated with different types.
+// 
+// (4). TypeFunction { parameters: Vec<Type>, return_type: Box<Type> } 
+//      may be represented by fn(<param1>, <param2>, ...) -> <return_type>.
+// 
+// (5). TypeQuantified(String) may be represented by <name>, representing a type
+//      that is universally quantified by function generics.
+//
+// <==== TYPES MODULE ====>
+
+use std::fmt::Debug;
+
+#[derive(Clone, PartialEq)]
+pub enum Type {
+    TypeIdentifier(String),
+    TypeApplication(Box<Type>, Vec<Type>),
+    TypeVariable(Box<TypeVariable>),
+    TypeFunction {
+      parameters: Vec<Type>,
+      return_type: Box<Type>,
+    },
+    TypeQuantified(String),
+}
+
+#[derive(Clone, PartialEq)]
+pub enum TypeVariable {
+    Unbound(String, usize),
+    Bound(Box<Type>),
+}
+
+impl Debug for TypeVariable {
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+      match self {
+          TypeVariable::Unbound(name, id) => write!(f, "{}{}", name, id),
+          TypeVariable::Bound(ty) => write!(f, "{:?}", ty),
+      }
+  }
+}
+
+impl Debug for Type {
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+      match self {
+          Type::TypeIdentifier(name) => write!(f, "{}", name),
+          Type::TypeApplication(base, args) => {
+            write!(f, "{:?}<", base)?;
+            for arg in args {
+                write!(f, "{:?}, ", arg)?;
+            }
+            write!(f, ">")
+          }
+          Type::TypeVariable(var) => write!(f, "{:?}", var),
+          Type::TypeFunction { parameters, return_type } => {
+            write!(f, "fn(")?;
+            for (i, param) in parameters.iter().enumerate() {
+                if i > 0 {
+                    write!(f, ", ")?;
+                }
+                write!(f, "{:?}", param)?;
+            }
+            write!(f, ") -> {:?}", return_type)
+          }
+          Type::TypeQuantified(name) => write!(f, "{}", name),
+      }
+  }
+}
