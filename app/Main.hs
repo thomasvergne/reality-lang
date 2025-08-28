c00cdc24353c204eab015d1741207102cf6d4b4f
Thomas Vergne <contact@thomas-vergne.fr>
2025-08-28 14:39:20 +0200

    feat(main): added import resolving in main entry

--- app/Main.hs
+++ app/Main.hs
@@ -4,16 +4,24 @@ import Control.Color
 import Control.Monad.Result
 import Language.Reality.Frontend.Parser hiding (parseError)
 import Language.Reality.Frontend.Parser.Toplevel qualified as T
+import Language.Reality.Frontend.Import.Resolver qualified as IR
+import System.Directory
+import System.FilePath
 
 main :: IO ()
 main = do
-    let file = "examples/structures.rl"
+    cwd <- (</> "examples") <$> getCurrentDirectory
+    let file = "examples/main.rl"
     fileContent :: Text <- decodeUtf8 <$> readFileBS file
 
     result <- parseRealityFile file fileContent T.parseProgram
 
     case result of
         Right ast -> do
-            mapM_ printText ast
+            irResult <- runExceptT $ IR.runImportResolver cwd ast
+
+            handle irResult $ \ir -> do
+                mapM_ printText ir
+
         Left err -> do
             parseError err file (Just fileContent)
