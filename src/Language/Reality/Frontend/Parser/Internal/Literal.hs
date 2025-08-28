73a275c906e62ac4ebc4d2d7e9b6cd65ac3a5a9e
Thomas Vergne <contact@thomas-vergne.fr>
2025-08-28 10:55:40 +0200

    feat(parser/internal): added Type and Literal parsers

---
+++ src/Language/Reality/Frontend/Parser/Internal/Literal.hs
@@ -0,0 +1,67 @@
+module Language.Reality.Frontend.Parser.Internal.Literal where
+
+import Data.Text qualified as T
+import Language.Reality.Frontend.Parser qualified as P
+import Language.Reality.Frontend.Parser.Lexer qualified as Lex
+import Language.Reality.Syntax.HLIR qualified as HLIR
+import Text.Megaparsec.Char (digitChar)
+import Text.Megaparsec.Char qualified as MC
+
+-- | DECIMAL
+-- | Parse a decimal number with optional underscores as separators.
+decimal :: (MonadIO m) => P.Parser m Integer
+decimal = do
+    parts :: String <- concat <$> P.sepBy1 (some digitChar) (MC.char '_')
+
+    case readEither parts of
+        Left _ -> fail "Invalid integer"
+        Right x -> pure x
+
+-- | INTEGER
+-- | Parse an integer literal.
+parseInteger :: (MonadIO m) => P.Parser m Integer
+parseInteger = P.signed (pure ()) decimal
+
+-- | FLOAT
+-- | Parse a floating-point number with optional underscores as separators.
+parseFloat :: (MonadIO m) => P.Parser m Double
+parseFloat = P.signed (pure ()) $ do
+    int <- concat <$> P.sepBy1 (some digitChar) (MC.char '_')
+    void $ MC.char '.'
+    frac <- concat <$> P.sepBy1 (some digitChar) (MC.char '_')
+
+    case readEither (int <> "." <> frac) of
+        Left _ -> fail "Invalid float"
+        Right y -> pure y
+
+-- | CHARACTER
+-- | Parse a character literal.
+parseChar :: (MonadIO m) => P.Parser m Char
+parseChar = MC.char '\'' *> P.charLiteral <* MC.char '\''
+
+-- | STRING
+-- | Parse a string literal.
+parseString :: (MonadIO m) => P.Parser m Text
+parseString = MC.char '"' *> P.manyTill P.charLiteral (MC.char '"') <&> T.pack
+
+-- | LITERAL
+-- | Parse a literal.
+-- | Literals are the most basic form of data in a programming language. They
+-- | are also called atomic or primitive values. Literals are used to represent
+-- | numbers, strings, characters, booleans, and other basic types.
+-- |
+-- | - Int: An integer literal of the form 0, 1, 2, 3, etc.
+-- | - Float: A floating-point literal of the form 0.0, 1.0, 2.0, 3.0, etc.
+-- | - Char: A character literal of the form 'a', 'b', 'c', etc.
+-- | - String: A string literal of the form "hello", "world", "bonzai", etc.
+-- | - Bool: A boolean literal of the form true or false.
+parseLiteral :: (MonadIO m) => P.Parser m HLIR.Literal
+parseLiteral =
+    P.choice
+        [ P.try $ HLIR.MkLitFloat <$> parseFloat
+        , P.try $ HLIR.MkLitInt <$> parseInteger
+        , P.try $ HLIR.MkLitBool True <$ Lex.reserved "true"
+        , P.try $ HLIR.MkLitBool False <$ Lex.reserved "false"
+        , HLIR.MkLitChar <$> parseChar
+        , HLIR.MkLitString <$> parseString
+        ]
---
+++ src/Language/Reality/Frontend/Parser/Internal/Type.hs
@@ -0,0 +1,42 @@
+module Language.Reality.Frontend.Parser.Internal.Type where
+
+import Language.Reality.Frontend.Parser qualified as P
+import Language.Reality.Frontend.Parser.Lexer qualified as Lex
+import Language.Reality.Syntax.HLIR qualified as HLIR
+
+-- | TYPE
+-- | Parse a type.
+parseType :: (MonadIO m) => P.Parser m (HLIR.Position, HLIR.Type)
+parseType =
+    P.choice
+        [ -- Function type constructor
+          -- Defined as the following:
+          --
+          -- "fn" "(" type ("," type)* ")" "->" type
+          do
+            ((start, _), _) <- Lex.reserved "fn"
+            tys <- snd <$> Lex.parens (P.sepBy (snd <$> parseType) Lex.comma)
+            ((_, end), ret) <- Lex.symbol ":" *> parseType
+
+            pure ((start, end), tys HLIR.:->: ret)
+        , -- Tuple type constructor
+          -- Defined as the following:
+          --
+          -- "(" type "," type ")"
+          do
+            (pos, ty) <- Lex.parens $ do
+                x <- snd <$> parseType
+                void $ Lex.reserved ","
+                HLIR.MkTyTuple x . snd <$> parseType
+            pure (pos, ty)
+        , -- Type application constructor
+          -- Defined as the following:
+          --
+          -- identifier "<" type ("," type)* ">"
+          P.try $ do
+            ((start, _), idt) <- Lex.identifier
+            ((_, end), tys) <- Lex.brackets $ P.sepBy1 (snd <$> parseType) Lex.comma
+
+            pure ((start, end), HLIR.MkTyApp (HLIR.MkTyId idt) tys)
+        , Lex.identifier <&> second HLIR.MkTyId
+        ]
