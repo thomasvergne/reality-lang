501df4ac147f1f656f1125a91d04b5b6cc82ea99
Thomas Vergne <contact@thomas-vergne.fr>
2025-08-29 19:13:11 +0200

    feat(main): updated example file

--- app/Main.hs
+++ app/Main.hs
@@ -2,19 +2,19 @@ module Main where
 
 import Control.Color
 import Control.Monad.Result
+import Language.Reality.Backend.Specialization.Resolver qualified as SR
 import Language.Reality.Frontend.Import.Resolver qualified as IR
 import Language.Reality.Frontend.Module.Resolver qualified as MR
-import Language.Reality.Frontend.Typechecker.Checker qualified as TC
-import Language.Reality.Backend.Specialization.Resolver qualified as SR
 import Language.Reality.Frontend.Parser hiding (parseError)
 import Language.Reality.Frontend.Parser.Toplevel qualified as T
+import Language.Reality.Frontend.Typechecker.Checker qualified as TC
 import System.Directory
 import System.FilePath
 
 main :: IO ()
 main = do
     cwd <- (</> "examples") <$> getCurrentDirectory
-    let file = "examples/main.rl"
+    let file = "examples/structures.rl"
     fileContent :: Text <- decodeUtf8 <$> readFileBS file
 
     result <- parseRealityFile file fileContent T.parseProgram
@@ -34,6 +34,5 @@ main = do
 
                         handle srResult $ \slir -> do
                             mapM_ printText slir
-
         Left err -> do
             parseError err file (Just fileContent)
