541df6035cee2351b477aa7bc7fe9f405daec758
Thomas Vergne <contact@thomas-vergne.fr>
2025-08-30 17:03:22 +0200

    feat(libc): updated basic C standard library of Reality

--- libc/String.c
+++ libc/String.c
@@ -4,6 +4,10 @@
 #include <stdlib.h>
 #include "../dependencies/gc/src/gc.h"
 
+GarbageCollector* get_gc() {
+    return &gc;
+}
+
 char* malloc_string(const char* content) {
     size_t length = strlen(content);
     char* copy = (char*)gc_malloc(&gc, length + 1);
@@ -19,6 +23,16 @@ char* number_to_string(int number) {
     char* result = (char*)gc_malloc(&gc, length + 1);
     if (result) {
         snprintf(result, length + 1, "%d", number);
+    }
+    return result;
+}
+
+char* pointer_to_string(void* ptr) {
+    size_t length = snprintf(NULL, 0, "%p", ptr);
+
+    char* result = (char*)gc_malloc(&gc, length + 1);
+    if (result) {
+        snprintf(result, length + 1, "%p", ptr);
     }
     return result;
 }
@@ -45,4 +59,38 @@ int sub_number(int a, int b) {
 
 bool equals_number(int a, int b) {
     return a == b;
+}
+
+char* fetch_ptr(char* container, uint32_t index) {
+    return &container[index];
+}
+
+char* ptr_add(char* ptr, uint32_t offset) {
+    return ptr + offset;
+}
+
+int greater_number(int a, int b) {
+    return a > b;
+}
+
+int less_number(int a, int b) {
+    return a < b;
+}
+
+int mod_number(int a, int b) {
+    if (b != 0) {
+        return a % b;
+    }
+    // Handle division by zero case
+    return 0;
+}
+
+char* concat_strings(char* a, char* b) {
+    size_t length = strlen(a) + strlen(b);
+    char* result = (char*)gc_malloc(&gc, length + 1);
+    if (result) {
+        strcpy(result, a);
+        strcat(result, b);
+    }
+    return result;
 }
