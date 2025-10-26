ad028a01f2eff5c456a54aa81233b041178f34d8
Thomas Vergne <contact@thomas-vergne.fr>
2025-10-26 21:57:09 +0100

    build(scripts): added installation script

---
+++ scripts/install.py
@@ -0,0 +1,46 @@
+from os import system
+from shutil import which
+import os.path
+from glob import glob
+import platform
+from sys import argv
+
+# Check for Cabal
+if not which('cabal'):
+  print('Please install cabal to build Reality.')
+  exit(1)
+
+# Build the compiler project
+system('cabal build exe:reality')
+
+ext = '.exe' if platform.system() == 'Windows' else ''
+
+executable_name = f"reality{ext}"
+
+found_executables = glob(f"dist-newstyle/**/{executable_name}", recursive=True)
+executable_files = [file for file in found_executables if os.path.isfile(file)]
+
+if len(executable_files) == 0:
+  print('No executable found')
+  exit(1)
+
+executable = executable_files[0]
+executable_out = f"rlc{ext}"
+
+if not os.path.isdir('bin'): os.mkdir('bin')
+
+system(f"cp {executable} bin/{executable_out}")
+
+print(f"Reality compiler installed to bin/{executable_out}")
+
+# Installing LF project manager
+
+cmd = f"bin/rlc{ext} packages/lf/main.rl -p std=$REALITY_DIR/std"
+
+system(cmd)
+
+gcc_cmd = f"gcc packages/lf/main.c $REALITY_DIR/libc/String.c $REALITY_DIR/libc/Actor.c $LIBGC/lib/libgc.a -I$LIBGC/include/ packages/lf/libc/System.c -lm -w -o bin/lf{ext} -DRELEASE_MODE -O3"
+
+system(gcc_cmd)
+
+print("LF project manager installed to packages/lf/output/program")
